b2014aa8a447183ee4733b45063f99c889df966a
yurist38 <yurist38@mail.ru>
2018-08-20 10:48:11 +0200

    fix: PR feedback

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node": true,
-    "jest": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
   },
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "defaultParams": true
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      defaultParams: true
     }
   },
-  "extends": [
-    "eslint:recommended",
-    "airbnb",
-    "plugin:react/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:jsx-a11y/recommended"
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended'
   ]
 };
