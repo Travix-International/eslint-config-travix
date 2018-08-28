857c9970ccc3e5520296869ea08eac1e2121e606
Yuri Drabik <yurist38@mail.ru>
2018-08-28 10:41:25 +0200

    minor: added singlequotes and some more rules to basic eslint config

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
-    ecmaFeatures: {
-      defaultParams: true,
-    },
   },
   extends: [
-    'eslint:recommended',
     'airbnb',
     'plugin:react/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:jsx-a11y/recommended',
   ],
+  rules: {
+    'max-len': ['error', { 'code': 120 }],
+    'react/jsx-sort-props': [1],
+    'react/forbid-prop-types': [1, { 'forbid': ['any'] }],
+  }
 };
